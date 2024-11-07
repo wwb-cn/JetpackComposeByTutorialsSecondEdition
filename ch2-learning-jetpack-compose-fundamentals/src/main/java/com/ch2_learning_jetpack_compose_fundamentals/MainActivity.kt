2feac0e3ccdca0af23885970be9eb50753b170e5
wwb-cn <wwb.cn@outlook.com>
2024-11-07 20:58:03 +0800

    ch2 initial code

---
+++ ch2-learning-jetpack-compose-fundamentals/src/main/java/com/ch2_learning_jetpack_compose_fundamentals/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ch2_learning_jetpack_compose_fundamentals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ch2_learning_jetpack_compose_fundamentals.ui.theme.JetpackComposeByTutorialsSecondEditionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeByTutorialsSecondEditionTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeByTutorialsSecondEditionTheme {
+        Greeting("Android")
+    }
+}
